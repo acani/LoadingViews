7d62d018f46294dda9f7ef688aab8053cec2607d
Matthew Di Pasquale <liveloveprosper@gmail.com>
2016-11-17 08:16:59 -0500

    addConstraints > NSLayoutConstraint.activate

--- LoadingViews/LoadingView.swift
+++ LoadingViews/LoadingView.swift
@@ -36,7 +36,7 @@ open class LoadingView: UIView {
 
         translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        addConstraints([
+        NSLayoutConstraint.activate([
             NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: titleLabel, attribute: .trailing, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: activityIndicatorView, attribute: .height, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: titleLabel, attribute: .leading, relatedBy: .equal, toItem: self, attribute: .leading, multiplier: 1, constant: width1+4),
@@ -51,7 +51,7 @@ open class LoadingView: UIView {
 
         let view = viewController.view
         view?.addSubview(self)
-        view?.addConstraints([
+        NSLayoutConstraint.activate([
             NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0),
             NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: viewController.topLayoutGuide.length)
         ])
